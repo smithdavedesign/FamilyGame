2158d8a022a04462e939e70ec3e336e86c7951b8
David Smith <smith36@seawolf.sonoma.edu>
2020-06-10 21:10:12 -0700

    register example

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-'use strict';
+const express = require('express')
+const app = express()
 
-console.log('Hello world');
+app.get('/register', function (req, res) {
+    //request 
+    req.body //last name , firstname {}
+    var carID = req.query.carID;
 
-//hello world
+
+})
+
+app.listen(3000)
